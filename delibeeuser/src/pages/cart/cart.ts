5524d02f846f404ae9fc9c691d3e821c8cd403e7
DESKTOP-BBPH7B4\shah.baizid.arefin <razen@golpocom.com>
2019-12-01 18:53:43 +0600

    route api coupon controller added cart.ts discount page updated

--- delibeeuser/src/pages/cart/cart.ts
+++ delibeeuser/src/pages/cart/cart.ts
@@ -35,8 +35,6 @@ export class CartPage {
   private couponAmount: string = '0';
   private couponApplied: boolean = false;
   private discount: number = 0;
-  private cart_qty: number = 0;
-  private item_qty: number = 0;
   private subscriptions: Array<Subscription> = [];
   private storeSelected: Store;
 
@@ -52,7 +50,7 @@ export class CartPage {
     if (cartItems != null) {
       this.cartItems = this.cartItems.concat(cartItems);
     }
-    this.calculateTotal(false);
+    this.calculateTotal();
 
     let address: Address = JSON.parse(window.localStorage.getItem(Constants.SELECTED_ADDRESS));
     this.storeSelected = JSON.parse(window.localStorage.getItem(Constants.SELECTED_STORE));
@@ -61,7 +59,7 @@ export class CartPage {
         console.log("calculateDeliveryFee", res);
         if (Number(res.delivery_fee) > 0) {
           this.delivery_fee = Number(res.delivery_fee);
-          this.calculateTotal(false);
+          this.calculateTotal();
         }
       }, err => {
         console.log("calculateDeliveryFeeErr", err);
@@ -79,7 +77,7 @@ export class CartPage {
   removeItem(product) {
     this.global.removeCartItem(product);
     this.cartItems = this.global.getCartItems();
-    this.calculateTotal(false);
+    this.calculateTotal();
   }
 
   decrementItem(product) {
@@ -87,7 +85,7 @@ export class CartPage {
     if (decremented) {
       if (!this.cartItems || !this.cartItems.length) {
         this.navCtrl.setRoot(HomePage)
-      } else this.calculateTotal(false);
+      } else this.calculateTotal();
     }
     let available: boolean = this.global.isItemAdded(product);
     this.cartItems = this.global.getCartItems();
@@ -100,7 +98,7 @@ export class CartPage {
     let incremented: boolean = this.global.incrementCartItem(product);
     if (incremented) {
       this.total = Number((this.total + Number(product.price)).toFixed(2));
-      this.calculateTotal(false);
+      this.calculateTotal();
     }
     this.cartItems = this.global.getCartItems();
     // this.translate.get(['itm_updated', 'itm_max_err']).subscribe(text => {
@@ -108,7 +106,7 @@ export class CartPage {
     // });
   }
 
-  calculateTotal(couponApplied) {
+  calculateTotal() {
     this.cartItems = this.global.getCartItems();
     let sum: number = 0;
     let offerAmmount: number = 0
@@ -124,18 +122,16 @@ export class CartPage {
     if (this.tax_in_percent) {
       this.totalServiceFee = Number(((sum * Number(this.tax_in_percent)) / 100).toFixed(2));
     }
-    if (couponApplied || this.couponApplied) {
-      let couponCode: Coupon = JSON.parse(window.localStorage.getItem(Constants.SELECTED_COUPON));
-      if (couponCode) {
-        this.coupon = couponCode;
-        this.couponAmount = this.coupon.reward + (this.coupon.type == 'percent' ? '%' : '');
-        this.discount = (this.coupon ? this.coupon.type == 'percent' ? (sum * Number(this.coupon.reward) / 100) : Number(this.coupon.reward) : 0);
-        console.log("discount is:--" + this.discount);
-        offerAmmount = sum - this.discount;
-        this.couponApplied = true;
-        this.total = Number((offerAmmount + this.totalServiceFee + this.delivery_fee).toFixed(2));
-      }
+
+    if (this.coupon) {
+      this.couponAmount = this.coupon.reward + (this.coupon.type == 'percent' ? '%' : '');
+      this.discount = (this.coupon ? this.coupon.type == 'percent' ? (sum * Number(this.coupon.reward) / 100) : Number(this.coupon.reward) : 0);
+      console.log("discount is:--" + this.discount);
+      offerAmmount = sum - this.discount;
+      this.total = Number((offerAmmount + this.totalServiceFee + this.delivery_fee).toFixed(2));
     } else {
+      this.discount = 0;
+      this.couponAmount = "0";
       this.total = Number((sum + this.totalServiceFee + this.delivery_fee).toFixed(2));
     }
   }
@@ -145,7 +141,12 @@ export class CartPage {
       let user: UserResponse = JSON.parse(window.localStorage.getItem(Constants.KEY_USER));
       if (user != null) {
         if (this.storeSelected && this.total >= this.storeSelected.minimum_order) {
-          this.navCtrl.push(ShippingPage, { discount: this.discount });
+          if (this.coupon) {
+            window.localStorage.setItem(Constants.SELECTED_COUPON, JSON.stringify(this.coupon));
+          } else {
+            window.localStorage.removeItem(Constants.SELECTED_COUPON);
+          }
+          this.navCtrl.push(ShippingPage, { coupon: this.coupon ? this.coupon.code : null, discount: this.discount });
         } else {
           this.translate.get('minimum_order').subscribe(value => {
             this.global.showToast(value + " " + this.storeSelected.minimum_order);
@@ -167,10 +168,23 @@ export class CartPage {
   }
 
   codePage() {
-    let modal = this.modalCtrl.create(CodePage);
-    modal.onDidDismiss(() => {
-      this.calculateTotal(true);
-    });
-    modal.present();
+    let user: UserResponse = JSON.parse(window.localStorage.getItem(Constants.KEY_USER));
+    if (user != null) {
+      this.coupon = null;
+      let modal = this.modalCtrl.create(CodePage);
+      modal.onDidDismiss((data) => {
+        this.coupon = data;
+        this.calculateTotal();
+      });
+      modal.present();
+    } else {
+      window.localStorage.setItem(Constants.TEMP_OPEN, Constants.TEMP_OPEN_CART);
+      this.translate.get('login2next').subscribe(value => {
+        this.global.showToast(value);
+      });
+      this.viewCtrl.dismiss();
+      this.navCtrl.push(SigninPage);
+    }
   }
+
 }
